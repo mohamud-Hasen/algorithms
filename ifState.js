e759fa5dcd21cc1f90d6685b4d27209547939de2
Mohamud E Hasen <mohmudes4@gmail.com>
2023-06-03 17:25:04 -0500

    added if else statement and ternary operator

---
+++ ifState.js
@@ -0,0 +1,15 @@
+
+let score = 60;
+if (score >= 90){
+    console.log('you got A.');
+}else if (score >= 80) {
+    console.log('you got B.');
+}else if (score >= 70){
+    console.log('you got C.');
+}else if (score >= 60){
+    console.log('you got D.');
+}else {
+    console.log('you failed the exam');
+}
+
+
---
+++ ternary.js
@@ -0,0 +1,5 @@
+let number = 2;
+number == 1 ? console.log('one')
+: number == 2 ? console.log('two')
+: number == 3 ? console.log('three')
+: console.log('none');
