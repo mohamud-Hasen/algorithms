32627709751f67bd852e82c2d49c559e8fcc7583
Mohamud E Hasen <mohmudes4@gmail.com>
2023-06-06 20:16:26 -0500

    an other way of solving longest substring in js

--- longestSubstring.js
+++ longestSubstring.js
@@ -6,25 +6,48 @@
 // If the character is in the set, remove the character at the start pointer from the set and increment start by 1.
 // Repeat the process until the end pointer reaches the end of the string.
 
-function longestSubstringWithoutDuplicates(str) {
-    let start = 0;
-    let end = 0;
-    let maxLength = 0;
-    let uniqueChars = new Set();
+// function longestSubstringWithoutDuplicates(str) {
+//     let start = 0;
+//     let end = 0;
+//     let maxLength = 0;
+//     let uniqueChars = new Set();
 
-    while(end < str.length) {
-        if(!uniqueChars.has(str.charAt(end))) {
-            uniqueChars.add(str.charAt(end));
-            maxLength = Math.max(maxLength, end - start + 1);
-            end++;
-        }else {
-            uniqueChars.delete(str.charAt(start));
-            start++;
+//     while(end < str.length) {
+//         if(!uniqueChars.has(str.charAt(end))) {
+//             uniqueChars.add(str.charAt(end));
+//             maxLength = Math.max(maxLength, end - start + 1);
+//             end++;
+//         }else {
+//             uniqueChars.delete(str.charAt(start));
+//             start++;
+//         }
+//     }
+//     return maxLength;
+    
+// }
+
+// Brute Force Approach:
+// Generate all possible substrings and check each substring for duplicate characters.
+// Keep track of the maximum length encountered.
+function longestSubstring2(str) {
+    let maxLength = 0;
+    for(let i = 0; i < str.length; i++) {
+        let uniqueChars = new Set();
+        let currentLength = 0;
+        for(let j = 0; j < str.length; j++) {
+            if(uniqueChars.has(str.charAt(j))) {
+                break;
+            }
+            uniqueChars.add(str.charAt(j));
+            currentLength++;
         }
+      maxLength = Math.max(maxLength, currentLength);
     }
     return maxLength;
-    
 }
-const s = "abcabcbb";
-console.log(longestSubstringWithoutDuplicates("abcabcbb"));
-console.log(longestSubstringWithoutDuplicates("pwwkew"));
+
+
+
+console.log(longestSubstring2("abcabcbb"));
+console.log(longestSubstring2("pwwkew"));
+console.log(longestSubstring2("bbbbb"));
