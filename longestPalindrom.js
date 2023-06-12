7fbaecd7f11b5d6cb1ba4676afa7018cdb1403ea
Mohamud E Hasen <mohmudes4@gmail.com>
2023-06-12 17:36:36 -0500

    changes made

--- longestPalindrom.js
+++ longestPalindrom.js
@@ -20,18 +20,38 @@
   
 //   const input = 'babad';
 //   console.log(longestPalindromicSubstring(input));
+
+
+
+// 5. Longest Palindromic Substring
+
+// Given a string s, return the longest 
+// palindromic
+ 
+// substring
+//  in s.
+
+ 
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+
+
   
-
-
-
-
-
-function longestPalindromicString(s) {
+function longestPalindromicSubstring(s) {
     let longest = '';
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i; j < s.length; j++) {
-            const substring = s.slice(i, j + 1);
-            if(isPalindrome(substring) && substring.length > longest.length) {
+    for(let i = 0; i < s.length; i++) {
+        for(let j = i; j < s.length; j++) {
+            let substring = s.slice(i, j + 1);
+            if(isPalindrome(substring) && substring.length > longest.length){
                 longest = substring;
             }
         }
@@ -42,5 +62,8 @@ function isPalindrome(s) {
     let reversed = s.split('').reverse().join('');
     return s === reversed;
 }
-const input = 'babad';
-console.log(longestPalindromicString(input));
+const s = 'cbbd';
+console.log(longestPalindromicSubstring(s));
+
+
+
