f59765e1af1db9493a9cff22bacc9dd1faf76c58
Mohamud E Hasen <mohmudes4@gmail.com>
2023-06-12 00:47:50 -0500

    added longest substring

--- longestPalindrom.js
+++ longestPalindrom.js
@@ -1,24 +1,46 @@
-function longestPalindromicSubstring(s) {
+// function longestPalindromicSubstring(s) {
+//     let longest = '';
+  
+//     for (let i = 0; i < s.length; i++) {
+//       for (let j = i; j < s.length; j++) {
+//         const substring = s.slice(i, j + 1);
+//         if (isPalindrome(substring) && substring.length > longest.length) {
+//           longest = substring;
+//         }
+//       }
+//     }
+  
+//     return longest;
+//   }
+  
+//   function isPalindrome(s) {
+//     const reversed = s.split('').reverse().join('');
+//     return s === reversed;
+//   }
+  
+//   const input = 'babad';
+//   console.log(longestPalindromicSubstring(input));
+  
+
+
+
+
+
+function longestPalindromicString(s) {
     let longest = '';
-  
     for (let i = 0; i < s.length; i++) {
-      for (let j = i; j < s.length; j++) {
-        const substring = s.slice(i, j + 1);
-        if (isPalindrome(substring) && substring.length > longest.length) {
-          longest = substring;
+        for (let j = i; j < s.length; j++) {
+            const substring = s.slice(i, j + 1);
+            if(isPalindrome(substring) && substring.length > longest.length) {
+                longest = substring;
+            }
         }
-      }
     }
-  
     return longest;
-  }
-  
-  function isPalindrome(s) {
-    const reversed = s.split('').reverse().join('');
+}
+function isPalindrome(s) {
+    let reversed = s.split('').reverse().join('');
     return s === reversed;
-  }
-  
-  const input = 'babad';
-  console.log(longestPalindromicSubstring(input));
-
-
+}
+const input = 'babad';
+console.log(longestPalindromicString(input));
